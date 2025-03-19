64bc6f40bf1acfb6e9b654f861fb9b62824f1252
Daniel Pilich <dpilich16@gmail.com>
2025-03-19 15:41:54 +0100

    feat: Add gravity

--- dasher.cpp
+++ dasher.cpp
@@ -8,6 +8,16 @@ int main()
 
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
+    const int gravity{1};
+
+    // Rectangle dimensions and properties
+    const int rectangleWidth{50};
+    const int rectangleHeight{80};
+
+    int rectangleX{windowWidth / 2 - rectangleWidth};
+    int rectangleY{windowHeight - rectangleHeight};
+    int rectangleVelocity{0};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -15,8 +25,28 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        DrawRectangle(rectangleX, rectangleY, rectangleWidth, rectangleHeight, BLUE);
+
+
+        if (rectangleY >= windowHeight - rectangleHeight)
+        {
+            // Rectangle on the ground
+            rectangleVelocity = 0;
+        }
+        else{
+            // Rectangle in the air            
+            rectangleVelocity += gravity;
+        }
+
+        
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            rectangleVelocity -= 10;
+        }
+
+        rectangleY += rectangleVelocity;
+
         EndDrawing();
     }
-
     CloseWindow();
 }
