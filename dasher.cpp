cf66e620e540e378512bc968d2c3f61a7c5ac19c
Daniel Pilich <dpilich16@gmail.com>
2025-03-25 14:29:56 +0100

    feat: Add obstacle animation

--- dasher.cpp
+++ dasher.cpp
@@ -10,27 +10,33 @@ int main()
 
     const int gravity{2'000}; // (pixels / second) / second
 
+    // Obstacle parameters
     Texture2D obstacle = LoadTexture("textures/obstacle.png");
     Rectangle obstacleRectangle{0.0f, 0.0f, obstacle.width / 8, obstacle.height / 8};
     Vector2 obstaclePosition{windowWidth, windowHeight - obstacleRectangle.height};
-    int obstacleVelocity{-600};
+    int obstacleVelocity{-200};
+    int obstacleAnimationFrame{0};
+    int obstacleAnimationLoop{1};
+    float obstacleRunningTime{0};
+    const float obstacleUpdateTime{1.0f / 12.0f};
 
+    // Hero parameters
     Texture2D hero = LoadTexture("textures/hero.png");
     Rectangle heroRectangle;
-    heroRectangle.width = hero.width/6;
+    heroRectangle.width = hero.width / 6;
     heroRectangle.height = hero.height;
     heroRectangle.x = 0;
     heroRectangle.y = 0;
     Vector2 heroPosition;
-    heroPosition.x = windowWidth/2 - heroRectangle.width/2;
+    heroPosition.x = windowWidth / 2 - heroRectangle.width / 2;
     heroPosition.y = windowHeight - heroRectangle.height;
-    int heroAnimationFrame{0};
     int heroVelocity{0};
+    int heroAnimationFrame{0};
+    float heroRunningTime{0};
+    const float heroUpdateTime{1.0f / 12.0f};
 
     bool isInAir{false};
-    float runningTime{0};
-    
-    const float updateTime{1.0f / 12.0f};
+
     const int jumpVelocity{1'000}; // pixels / second
 
     SetTargetFPS(60);
@@ -40,31 +46,50 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
-        const float deltaTime{ GetFrameTime() };
+        const float deltaTime{GetFrameTime()};
 
+        //Obstacle animation
+        obstacleRunningTime += deltaTime;
+        if (obstacleRunningTime >= obstacleUpdateTime)
+        {
+            obstacleRunningTime = 0;
+
+            // Update animation frame
+            obstacleRectangle.x = obstacleAnimationFrame * obstacleRectangle.width;
+            obstacleAnimationFrame += obstacleAnimationLoop;
+            if (obstacleAnimationFrame >= 7 || obstacleAnimationFrame <= 0)
+            {
+                obstacleAnimationLoop *= -1;
+            }
+        }
 
         DrawTextureRec(obstacle, obstacleRectangle, obstaclePosition, WHITE);
 
-        if(!isInAir){
-            runningTime += deltaTime;            
-            if(runningTime >= updateTime){
-                runningTime = 0;
-    
-                // Update animation frame  
+        // Hero animation
+        if (!isInAir)
+        {
+            heroRunningTime += deltaTime;
+            if (heroRunningTime >= heroUpdateTime)
+            {
+                heroRunningTime = 0;
+
+                // Update animation frame
                 heroRectangle.x = heroAnimationFrame * heroRectangle.width;
                 heroAnimationFrame++;
-                if(heroAnimationFrame > 5){
+                if (heroAnimationFrame > 5)
+                {
                     heroAnimationFrame = 0;
                 }
             }
         }
-        else{
+        else
+        {
             heroRectangle.x = 0 * heroRectangle.width;
         }
-        
 
         DrawTextureRec(hero, heroRectangle, heroPosition, WHITE);
 
+        // Hero movement
         if (heroPosition.y >= windowHeight - heroRectangle.height)
         {
             // Rectangle on the ground
@@ -87,6 +112,7 @@ int main()
 
         // Update obstacle position
         obstaclePosition.x += obstacleVelocity * deltaTime;
+
         // Update hero position
         heroPosition.y += heroVelocity * deltaTime;
 
