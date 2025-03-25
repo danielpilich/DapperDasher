24a206eba76282cd099fd8a74bc71982ff033a57
Daniel Pilich <dpilich16@gmail.com>
2025-03-25 15:18:37 +0100

    feat: Add second obstacle

--- dasher.cpp
+++ dasher.cpp
@@ -20,6 +20,14 @@ int main()
     float obstacleRunningTime{0};
     const float obstacleUpdateTime{1.0f / 12.0f};
 
+    
+    Rectangle obstacle2Rectangle{0.0f, 0.0f, obstacle.width / 8, obstacle.height / 8};
+    Vector2 obstacle2Position{windowWidth + 300, windowHeight - obstacleRectangle.height};
+    int obstacle2AnimationFrame{0};
+    int obstacle2AnimationLoop{1};
+    float obstacle2RunningTime{0};
+    const float obstacle2UpdateTime{1.0f / 16.0f};
+
     // Hero parameters
     Texture2D hero = LoadTexture("textures/hero.png");
     Rectangle heroRectangle;
@@ -63,7 +71,23 @@ int main()
             }
         }
 
-        DrawTextureRec(obstacle, obstacleRectangle, obstaclePosition, WHITE);
+        //Obstacle 2 animation
+        obstacle2RunningTime += deltaTime;
+        if (obstacle2RunningTime >= obstacle2UpdateTime)
+        {
+            obstacle2RunningTime = 0;
+
+            // Update animation frame
+            obstacle2Rectangle.x = obstacle2AnimationFrame * obstacle2Rectangle.width;
+            obstacle2AnimationFrame += obstacle2AnimationLoop;
+            if (obstacle2AnimationFrame >= 7 || obstacle2AnimationFrame <= 0)
+            {
+                obstacle2AnimationLoop *= -1;
+            }
+        }
+
+        DrawTextureRec(obstacle, obstacleRectangle, obstaclePosition, WHITE);        
+        DrawTextureRec(obstacle, obstacle2Rectangle, obstacle2Position, RED);
 
         // Hero animation
         if (!isInAir)
@@ -111,7 +135,9 @@ int main()
         }
 
         // Update obstacle position
-        obstaclePosition.x += obstacleVelocity * deltaTime;
+        obstaclePosition.x += obstacleVelocity * deltaTime;        
+        // Update obstacle 2 position
+        obstacle2Position.x += obstacleVelocity * deltaTime;
 
         // Update hero position
         heroPosition.y += heroVelocity * deltaTime;
