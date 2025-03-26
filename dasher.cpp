f6b408ca00e81626e63191dfd94ea10ac5bf4f6c
Daniel Pilich <dpilich16@gmail.com>
2025-03-26 14:34:58 +0100

    refactor: Add arrays for window dimensions and obstacles animation data

--- dasher.cpp
+++ dasher.cpp
@@ -7,15 +7,17 @@ struct AnimationData
     int frame;
     float updateTime;
     float runningTime;
+    int loop = 1;
 };
 
 int main()
 {
     // Window dimensions
-    const int windowWidth{1280};
-    const int windowHeight{720};
+    int windowDimensions[2];
+    windowDimensions[0] = 512;
+    windowDimensions[1] = 380;
 
-    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+    InitWindow(windowDimensions[0], windowDimensions[1], "Dapper Dasher");
 
     const int gravity{2'000}; // (pixels / second) / second
 
@@ -23,23 +25,23 @@ int main()
     Texture2D obstacle = LoadTexture("textures/obstacle.png");
     int obstacleVelocity{-200};
 
-    AnimationData obstacleData{
-        {0.0f, 0.0f, obstacle.width / 8, obstacle.height / 8}, // Rectangle rectangle
-        {windowWidth, windowHeight - obstacle.height / 8},     // Vector2 position
-        0,                                                     // int frame
-        1.0f / 12.0f,                                          // float updateTime
-        0.0f                                                   // float runningTime
-    };
-    int obstacleAnimationLoop{1};
-
-    AnimationData obstacle2Data{
-        {0.0f, 0.0f, obstacle.width / 8, obstacle.height / 8},   // Rectangle rectangle
-        {windowWidth + 300, windowHeight - obstacle.height / 8}, // Vector2 position
-        0,                                                       // int frame
-        1.0f / 16.0f,                                            // float updateTime
-        0.0f                                                     // float runningTime
-    };
-    int obstacle2AnimationLoop{1};
+    AnimationData obstaclesData[2]{
+        // Obstacle 1
+        {
+            {0.0f, 0.0f, obstacle.width / 8, obstacle.height / 8},            // Rectangle rectangle
+            {windowDimensions[0], windowDimensions[1] - obstacle.height / 8}, // Vector2 position
+            0,                                                                // int frame
+            1.0f / 12.0f,                                                     // float updateTime
+            0.0f                                                              // float runningTime
+        },
+        // Obstacle 2
+        {
+            {0.0f, 0.0f, obstacle.width / 8, obstacle.height / 8},                  // Rectangle rectangle
+            {windowDimensions[0] + 300, windowDimensions[1] - obstacle.height / 8}, // Vector2 position
+            0,                                                                      // int frame
+            1.0f / 16.0f,                                                           // float updateTime
+            0.0f                                                                    // float runningTime
+        }};
 
     // Hero parameters
     Texture2D hero = LoadTexture("textures/hero.png");
@@ -48,8 +50,8 @@ int main()
     heroData.rectangle.height = hero.height;
     heroData.rectangle.x = 0;
     heroData.rectangle.y = 0;
-    heroData.position.x = windowWidth / 2 - heroData.rectangle.width / 2;
-    heroData.position.y = windowHeight - heroData.rectangle.height;
+    heroData.position.x = windowDimensions[0] / 2 - heroData.rectangle.width / 2;
+    heroData.position.y = windowDimensions[1] - heroData.rectangle.height;
     heroData.frame = 0;
     heroData.updateTime = 1.0f / 12.0f;
     heroData.runningTime = 0.0f;
@@ -69,37 +71,37 @@ int main()
         const float deltaTime{GetFrameTime()};
 
         // Obstacle animation
-        obstacleData.runningTime += deltaTime;
-        if (obstacleData.runningTime >= obstacleData.updateTime)
+        obstaclesData[0].runningTime += deltaTime;
+        if (obstaclesData[0].runningTime >= obstaclesData[0].updateTime)
         {
-            obstacleData.runningTime = 0;
+            obstaclesData[0].runningTime = 0;
 
             // Update animation frame
-            obstacleData.rectangle.x = obstacleData.frame * obstacleData.rectangle.width;
-            obstacleData.frame += obstacleAnimationLoop;
-            if (obstacleData.frame >= 7 || obstacleData.frame <= 0)
+            obstaclesData[0].rectangle.x = obstaclesData[0].frame * obstaclesData[0].rectangle.width;
+            obstaclesData[0].frame += obstaclesData[0].loop;
+            if (obstaclesData[0].frame >= 7 || obstaclesData[0].frame <= 0)
             {
-                obstacleAnimationLoop *= -1;
+                obstaclesData[0].loop *= -1;
             }
         }
 
         // Obstacle 2 animation
-        obstacle2Data.runningTime += deltaTime;
-        if (obstacle2Data.runningTime >= obstacle2Data.updateTime)
+        obstaclesData[1].runningTime += deltaTime;
+        if (obstaclesData[1].runningTime >= obstaclesData[1].updateTime)
         {
-            obstacle2Data.runningTime = 0;
+            obstaclesData[1].runningTime = 0;
 
             // Update animation frame
-            obstacle2Data.rectangle.x = obstacle2Data.frame * obstacle2Data.rectangle.width;
-            obstacle2Data.frame += obstacle2AnimationLoop;
-            if (obstacle2Data.frame >= 7 || obstacle2Data.frame <= 0)
+            obstaclesData[1].rectangle.x = obstaclesData[1].frame * obstaclesData[1].rectangle.width;
+            obstaclesData[1].frame += obstaclesData[1].loop;
+            if (obstaclesData[1].frame >= 7 || obstaclesData[1].frame <= 0)
             {
-                obstacle2AnimationLoop *= -1;
+                obstaclesData[1].loop *= -1;
             }
         }
 
-        DrawTextureRec(obstacle, obstacleData.rectangle, obstacleData.position, WHITE);
-        DrawTextureRec(obstacle, obstacle2Data.rectangle, obstacle2Data.position, RED);
+        DrawTextureRec(obstacle, obstaclesData[0].rectangle, obstaclesData[0].position, WHITE);
+        DrawTextureRec(obstacle, obstaclesData[1].rectangle, obstaclesData[1].position, RED);
 
         // Hero animation
         if (!isInAir)
@@ -126,7 +128,7 @@ int main()
         DrawTextureRec(hero, heroData.rectangle, heroData.position, WHITE);
 
         // Hero movement
-        if (heroData.position.y >= windowHeight - heroData.rectangle.height)
+        if (heroData.position.y >= windowDimensions[1] - heroData.rectangle.height)
         {
             // Rectangle on the ground
             isInAir = false;
@@ -147,9 +149,9 @@ int main()
         }
 
         // Update obstacle position
-        obstacleData.position.x += obstacleVelocity * deltaTime;
+        obstaclesData[0].position.x += obstacleVelocity * deltaTime;
         // Update obstacle 2 position
-        obstacle2Data.position.x += obstacleVelocity * deltaTime;
+        obstaclesData[1].position.x += obstacleVelocity * deltaTime;
 
         // Update hero position
         heroData.position.y += heroVelocity * deltaTime;
